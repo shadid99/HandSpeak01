0b3e29a8d931cf8e3e533c9af5fe5e2f102b63e7
shadid99 <124088551+shadid99@users.noreply.github.com>
2026-08-30 02:50:23 +0300

    Add TEXT_TO_SIGN_ENDPOINT to config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,7 @@
 export const config = {
   BASE_URL: 'http://10.0.0.109:8000',
   PREDICTION_ENDPOINT: '/predict',
+  TEXT_TO_SIGN_ENDPOINT: '/text-to-sign',
   CAPTURE_INTERVAL: 2000,
   APP_VERSION: '1.0.0',
 };
